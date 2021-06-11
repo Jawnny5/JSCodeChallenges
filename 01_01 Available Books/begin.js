33566db385a2a007b3204dfdbcadbdee474c438c
“Jawnny5” <william.neal83@gmail.com>
2021-06-11 15:02:31 -0600

    creation of book class

---
+++ 01_01 Available Books/begin.js
@@ -0,0 +1,13 @@
+/*
+  WRITE YOUR SOLUTION HERE
+*/
+
+const Book = class {
+  constructor(title, author, isbn, numcopies){
+    this.title = title
+    this.author = author
+    this.isbn = isbn
+    this.numcopies = numcopies
+  }
+}
+ 
