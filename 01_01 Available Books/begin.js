95193bbdf1f46568d0bd4a0980b34f6d69a028a1
“Jawnny5” <william.neal83@gmail.com>
2021-06-11 15:05:00 -0600

    availability method and getter

--- 01_01 Available Books/begin.js
+++ 01_01 Available Books/begin.js
@@ -9,5 +9,25 @@ const Book = class {
     this.isbn = isbn
     this.numcopies = numcopies
   }
+
+  get availability(){
+    return this.getAvailbility()
+  }
+
+  getAvailbility(){
+    if(this.numcopies === 0){
+      return 'Out Of Stock'
+    } else if(this.numcopies < 10){
+      return 'Low Stock'
+    } return 'In Stock'
+  }
+
 }
+
+const harryPotter = new Book('Prisoner of Azkaban', 'That Lady', '42069', 11)
+
+console.log(harryPotter.availability)
+
+
+
  
